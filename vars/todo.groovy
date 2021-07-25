027078c4da4eab6c1327375ee415e03811221031
sravanizelar <sravani@zelarsoft.com>
2021-07-25 22:37:30 +0530

    ss

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -59,3 +59,25 @@ def call(Map params = [:]) {
 
     }
 }
+
+stage('Deploy to Dev Env') {
+    steps {
+        script {
+            get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+            env.get_branch_exec=sh(returnStdout: true, script: get_branch)
+        }
+        build job: 'Deployment Pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: "${get_branch_exec}")]
+    }
+}
+
+}
+
+}
+
+post {
+    always {
+        cleanWs()
+    }
+}
+
+}
